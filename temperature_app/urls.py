71d858a06ae88eabea2c5057e67cfa30b2bef0d8
aldreamex <mukhitov-2000@mail.com>
2023-10-10 13:58:25 +0300

    chart_update,create

--- temperature_app/urls.py
+++ temperature_app/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from temperature_app.views import temperature_chart
+from temperature_app.views import temperature_chart, chart_update
 
 urlpatterns = [
     path('chart/', temperature_chart, name='temperature_chart'),
+    path('chart_update/', chart_update, name='chart_update'),
 ]
--- temperature_app/views.py
+++ temperature_app/views.py
@@ -1,46 +1,36 @@
+import json
+from django.http import JsonResponse
 import matplotlib
-matplotlib.use('Agg')  # Agg для рендеринга графика
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
 from django.shortcuts import render
 from .models import TemperatureReading
 import pytz
+from .utils import create_chart
 
 
 def temperature_chart(request):
-    # Последние 5 записей изменения температуры
+    # Получаем последние 5 записей изменения температуры из БД
     readings = TemperatureReading.objects.order_by('-timestamp')[:5]
+    chart_data = create_chart(readings)
 
-
-    timezone = pytz.timezone('Europe/Moscow')
-    timestamps = [reading.timestamp.astimezone(timezone).strftime("%Y-%m-%d %H:%M:%S") for reading in readings]
-
-    temperatures = [reading.temperature for reading in readings]
-
-    # Стиль графика
-    plt.style.use('classic')
-
-    # График
-    fig, ax = plt.subplots(figsize=(10, 6))
-    ax.plot(timestamps, temperatures, marker='o', linestyle='-', color='b', label='Температура')
-    ax.set_xlabel('Дата/время')
-    ax.set_ylabel('Температура (°C)')
-    ax.set_title('График показаний температуры с датчика')
-    ax.grid(True)
-    ax.legend()
-
-    # Сохраняем график в памяти
-    buffer = BytesIO()
-    plt.savefig(buffer, format='png')
-    buffer.seek(0)
-    plt.close()
-
-    # Преобразуем изображение в строку
-    chart_data = base64.b64encode(buffer.read()).decode()
-    buffer.close()
 
     context = {
         'chart_data': chart_data
     }
+
     return render(request, 'temperature_chart.html', context)
+
+
+ # Еще один совет, функцию которая будет график тебе создавать вынеси отдельно от views  к примеру создай файл utils  и туда положи
+def chart_update(request):
+    readings = TemperatureReading.objects.order_by('-timestamp')[:5]
+    chart_data = create_chart(readings)
+    #json_data = json.dumps({'chart_data':chart_data}) - у тебя было так эта штука тож  json возвращает
+
+    return JsonResponse({'chart_data':chart_data}, safe=False)
+
+# крч надо вынести функционал создания графика в отдельную функцию, т.к. он у нас уже переиспользуется
+# потом в новой view это все заюзать и вернуть в виде  json
