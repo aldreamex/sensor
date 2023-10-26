25c56a6f3084ebb0099eba87c730a1eb4dccc024
Александр <mukhitov-2000@mail.ru>
2023-10-26 11:26:31 +0300

    fix urls

--- temperature_app/urls.py
+++ temperature_app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from temperature_app.views import temperature_chart, chart_update_data
 
 urlpatterns = [
-    path('chart/', temperature_chart, name='temperature_chart'),
+    path('', temperature_chart, name='temperature_chart'),
     path('chart_update_data/', chart_update_data, name='chart_update_data'),
 ]
