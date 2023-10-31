a5816d38d23e633c80a20d6124ae2d516df1de42
Александр <mukhitov-2000@mail.ru>
2023-10-31 13:53:28 +0300

    deploy

--- temperature_monitor/settings.py
+++ temperature_monitor/settings.py
@@ -87,17 +87,6 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'sensor_temp',
-#         'USER': 'postgres',
-#         'PASSWORD': 'sasha3240A1',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
